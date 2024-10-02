3f79d26f90fbf5b971316988b23bd0832541edfb
Aniket Kumar <kraniket123654@gmail.com>
2024-10-03 00:35:53 +0530

    yoo

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ wss.on('connection', (ws) => {
         }
       });
     }
+    if(data.type==='first_call'){
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: 'first_call' }));
+        }
+      });
+    }
   });
 
   ws.on('close', () => {
