6d6bd022606a672f95e47574e86fcec80df68260
harshdew02 <harshdewangan032@gmail.com>
2024-11-14 02:59:43 +0530

    student done

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { StudentRegistration, StudentLogin , EnrolledClasses, GetAttandaces} = require('../controllers/student.controller.js');
 const router = express.Router();
 const Attendance = require('../models/attendance.model.js');
+const Class = require('../models/class.model.js');
 
 router.get('/', (req, res) => {
     res.send('route frome student');
@@ -18,6 +19,35 @@ router.post('/otp', (req, res) => {
 router.get('/class-info/:studentRollNumber', EnrolledClasses );
 
 router.get('/attendance', GetAttandaces);
+
+
+router.get('/attendance/:rollNumber', async (req, res) => {
+    try {
+      const rollNumber = req.params.rollNumber;
+  
+      // Find attendance records for the given roll number
+      const attendanceRecords = await Attendance.find({
+        "records.rollNumber": rollNumber
+      }).populate('class_id');
+  
+      // Process the attendance data
+      const attendanceSummary = attendanceRecords.map(record => {
+        const className = record.class_id.classname;
+        const numberOfDatesP = record.records.filter(r => r.rollNumber === rollNumber && r.is_present).length;
+        const numberOfDatesA = record.records.filter(r => r.rollNumber === rollNumber && !r.is_present).length;
+  
+        return {
+          class_name: className,
+          numberOfDatesP,
+          numberOfDatesA
+        };
+      });
+  
+      res.json(attendanceSummary);
+    } catch (error) {
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
 
 module.exports = router
 
@@ -37,4 +67,12 @@ module.exports = router
 
 // teacher name include,  get student attendace vale me , class name include
 
-// 
+//  rollno -> array obj
+// {
+//     class_name,
+//     dates_present
+//     dates_absent
+// }
+
+
+
